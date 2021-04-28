763ee86ddb4f8b81fcbfb41e397d1ee64a4f3050
Dominik Mendoza <dominick9753@gmail.com>
2021-04-27 22:20:00 -0500

    Actualizacion 1

--- Ejercicio 5/COvni.cpp
+++ Ejercicio 5/COvni.cpp
@@ -1,1 +1,12 @@
 #include "COvni.h"
+
+void COvni::Borrar()
+{
+	for (int i = 0; i < l; i++)
+	{
+		for (int j = 0; j < a; j++)
+		{
+			Console::SetCursorPosition(x + i, y + j);
+		}
+	}
+}
--- Ejercicio 5/COvni.h
+++ Ejercicio 5/COvni.h
@@ -1,6 +1,26 @@
 #pragma once
+#include <iostream>
+using namespace std;
+using namespace System;
 class COvni
 {
-	
+protected:
+	int x, y, a, l;
+	int dx, dy, color;
+
+public:
+	COvni();
+	~COvni();
+	void Crear_Llantas(int cantidad);
+	void Borrar();
+	void Mostrar();
+	void Mover(int MAXW, int MAXH);
+	virtual void Cambiar_x(int nuevo);
+	void Cambiar_dx(int nuevo);
+	virtual void Cambiar_y(int nuevo);
+	void Detenerse();
+	int Retornar_x();
+	int Retornar_l();
+	int Retornar_dx();
 };
 
